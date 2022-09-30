c19eb725aa7a097c657b143677895617527692bf
vsulimovvv <super.sulimov2014@yandex.ru>
2022-09-30 15:35:46 +0300

    правки

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
+  // * ===== Mask input
+  $('input[type="tel"]').mask('+7 (999) 999-99-99');
+
   // * ===== Nice Select
   // $('select').niceSelect();
 
@@ -83,6 +86,40 @@ window.addEventListener('DOMContentLoaded', () => {
       overlay.classList.remove('active');
       body.classList.remove('no-scroll');
     });
+  })();
+
+  // * ===== Modal
+  (function modals() {
+    function bindModal(openBtn, modal, close) {
+      const openBtnEl = document.querySelectorAll(openBtn);
+      const modalEl = document.querySelector(modal);
+      const closeEl = document.querySelectorAll(close);
+      const body = document.querySelector('body');
+      if (modalEl) {
+        openBtnEl.forEach((el) => {
+          el.addEventListener('click', (e) => {
+            if (e.target) {
+              e.preventDefault();
+            }
+            modalEl.classList.add('active');
+            body.classList.add('no-scroll');
+          });
+        });
+        closeEl.forEach((btn) => {
+          btn.addEventListener('click', (e) => {
+            modalEl.classList.remove('active');
+            body.classList.remove('no-scroll');
+          });
+        });
+        modalEl.addEventListener('click', (e) => {
+          if (e.target === modalEl) {
+            modalEl.classList.remove('active');
+            body.classList.remove('no-scroll');
+          }
+        });
+      }
+    }
+    bindModal('.online-booking-btn', '.popup--online-booking', '.popup__close');
   })();
 
   // * ===== Toggle Tabs
